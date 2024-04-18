3cffcfc2902bef2740b10f161221e150bb85f100
Veronika Kolaříková <veronika.kolarikova@gnj.cz>
2024-04-18 19:46:54 +0200

    verze01

---
+++ script.js
@@ -0,0 +1,114 @@
+// document.body.textContent += 'něco něco' /* bere tupě text */
+
+
+//     /* Vstup */
+// const jmeno = prompt("Zadej jméno")
+
+// document.body.innerHTML += 'Ahoj uživateli:' + jmeno
+
+// const pocetHodin = Number(prompt("Zadej počet hodin zaměstnance")) // 100 spring konvertujeme pomocí Number
+// const pocetHodinPrescasu = Number(prompt("Zadej počet hodin přesčasů")) //v10 toString(), parseIn(), parseFloat()
+// const hodinovka = 200
+// const vypocetMzdy = (pocetHodin * hodinovka) + (pocetHodinPrescasu * hodinovka + 100)
+// const celkovyPocetHodin = pocetHodin + pocetHodinPrescasu
+
+// document.body.innerHTML += '<p>Zaměstnanec odpracoval - ' + celkovyPocetHodin1001 + 'hodin</p>'
+// document.body.innerHTML += '<p>Zaměstnanec dostane plat - ' + vypocetMzdy + "Kč.</p>"
+
+
+/*  Příklady - očkování */
+
+// const jmeno = prompt("Uveďte vaše jméno a příjmení:")
+// const vek = Number(prompt("Kolik je vám let:"))
+
+// document.body.innerHTML += jmeno + ',' + ' věk: ' + vek
+
+
+
+/*  Příklady - Výplata jako stránka */
+
+
+// const hodinovka = Number(prompt("Napište vaší hodinovou sazbu v korunách:"))
+// const den = 8
+// const mesíc = 21
+// const hrubaMzda = hodinovka * den * mesíc
+
+// document.body.innerHTML += 'Hrubá měsíční mzda činí ' + hrubaMzda + ' korun.'
+
+// const hodinovka = Number(prompt("Napište vaší hodinovou sazbu v korunách:"))
+// const den = Number(prompt("Kolik hodin denně pracujete?"))
+// const mesic = Number(prompt("Kolik dnů odpracujete za měsíc?"))
+// const hrubaMzda = hodinovka * den * mesic
+
+// document.body.innerHTML += 'Hrubá měsíční mzda činí ' + hrubaMzda + ' korun.'
+
+
+
+
+
+//const adresa = "Opletalova 33, 169 00 Praha 6"
+
+// const adresa = {
+//     //vlastnost : hodnota
+//     ulice: "Opletalova",
+//     "cislo popisne": 33,
+//     psc: "169 00",
+//     mesto: "Praha 6",
+//     gps: {
+//         x: 12312.1231224,
+//         y: 12314.123123
+//     }
+// }
+
+// adresa.ulice = "Fibichova"
+// adresa.ctvrt = "Dejvice"
+
+// document.body.innerHTML = "<p>Ulice: " + adresa.ulice + "</p>" + "<p>Cislo popisne: " + adresa["cislo popisne"] + "</p>"
+
+// console.log(adresa)
+// console.log(adresa.gps.y)
+
+
+// MOJE adresa 
+// const adresa = {
+// // vlastnost : hodnota
+//     ulice: "Opletalova",
+//     cisloPopisne: 33,
+// //  "cislo popisne": 33,
+//     psc: "169 00",
+//     mesto: "Praha 6"
+//     gps: {
+//         x: 12345.234567,
+//         y: 23458.977654
+//     }
+// }
+
+// adresa.ulice = 'Fibichova'
+// adresa.ctvrt = 'Dejvice'
+
+//document.body.innerHTML += '<p> Ulice: ' + adresa.ulice + '</p>'
+// document.body.innerHTML = "<p>Ulice: " + adresa.ulice + "</p>" + "<p>Cislo popisne: " + adresa.cisloPopisne + "</p>"
+
+//document.body.innerHTML = "<p>Ulice: " + adresa.ulice + "</p>" + "<p>Cislo popisne: " + adresa["cislo popisne"] + "</p>"
+
+//console.log(adresa)
+// console.log(adresa.gps.y)
+
+
+
+// const tridni = prompt("Zadej jméno třídního učitele:")
+// const nazevTridy = prompt("Zadej označeneí třídy:")
+// const trida = {}
+
+// trida.jmenoTridni = tridni
+// trida.jmenoTridy = nazevTridy
+
+// console.log(trida)
+
+// console.log(window)
+
+
+
+
+
+
